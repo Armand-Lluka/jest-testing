f01361c2a3519c3ff23c8827baa6c5498d2cc8bc
Armand-Lluka <armandwork@hotmail.co.uk>
2018-12-10 19:03:24 +0000

    isNull & checkValue

--- functions.test.js
+++ functions.test.js
@@ -3,3 +3,27 @@ const functions = require('./functions')
 test('Add 2+2 to equal 4', () =>{
     expect(functions.add(2,2)).toBe(4)
 })
+
+test('Add 2+2 to NOT equal 5', () =>{
+    expect(functions.add(2,2)).not.toBe(5)
+})
+
+// Check for TRUTHY and FALSY values
+
+// toBeNull matches only null 
+
+// toBeUndefined matches only undefined 
+
+// toBeDefined is the oppoiste of toBeUndefined
+
+// toBeTruthy matches anything that an if statement treats as true 
+
+// toBeFalsy matches anything that an if statement treats as false
+
+test('should return null', () =>{
+    expect(functions.isNull()).toBeNull()
+})
+
+test('should be falsy', () =>{
+    expect(functions.checkValue(0)).toBeFalsy()
+})
